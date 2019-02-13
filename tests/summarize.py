e4dd0c6d32e722aaaacbe47e1fb738e88751e8f2
WHGhost <wghosth@gmail.com>
2019-02-13 16:23:07 +0100

    summarize script was doing mre than it should

--- tests/summarize.py
+++ tests/summarize.py
@@ -37,24 +37,3 @@ for indice, chunk in enumerate(img.chunks):
     print("Report for chunk {}".format(indice))
     png_chunk_summary(chunk)
     print('='*50)
-
-
-for chunk in img.chunks:
-    chunk.type = chunk.type
-    chunk.update_crc()
-    len(chunk)
-    chunk.iscritical()
-    chunk.isancillary()
-    if not chunk.is_supported():
-        print(' '* 50, end='\r')
-        print("Found unsupported chunk: {} in {}".format(chunk.type, argv[1]))
-        continue
-    if chunk.type not in ('PLTE', 'IDAT'):
-        for key, val in chunk.get_payload().items():
-            try:
-                chunk[key] = val
-            except KeyError:
-                pass
-    chunk.is_valid()
-    chunk._set_empty_data()
-    img.reset()
